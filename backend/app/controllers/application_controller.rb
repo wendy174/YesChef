566c97d9fb5154e255fd067d80b7e98bb30b50f4
Liza McLain <lizamclain@gmail.com>
2023-02-14 17:03:39 -0600

    liza recipe get method for homepage

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    top_five = Recipe.top_five_kiss_count
+    top_five.to_json(only: [:title, :image_URL, :cuisine, :time, :difficulty])
+    # will try to figure out how to include kiss_count as an integer/variable
   end
 
 end

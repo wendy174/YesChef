bed95b1fcd93ce5622006d14370848cd7aacc81c
Mike <mike@mymodus.co>
2023-02-15 21:09:36 -0500

    card updates

--- frontend/src/components/RecipeCard.js
+++ frontend/src/components/RecipeCard.js
@@ -1,43 +1,43 @@
 import React from 'react';
 // import { NavLink } from "react-router-dom";
-import { Card , Button , Icon , Label } from "semantic-ui-react";
+import { Card , Button , Icon , Label , Image } from "semantic-ui-react";
 
 function RecipeCard ({ recipe }) {
 
 const { title, cuisine, time, image_URL, difficulty } = recipe;
 return (
     <Card>
-        <div className="recipe-card">
-            <div className="image">
-                <img style={{width: '230px', height: '250px'}}src= {image_URL} alt={title}/>
-            </div>
-            <div className="recipe-title">
-                <h2 className="header">{title}</h2>
-            </div>
-            <div className="recipe-details">
-                <h3>{cuisine}</h3>
-                <h3>{time} minutes</h3>
-                <h3>{difficulty}</h3>
-            </div>
-            <Button as='div' labelPosition='right'>
-                <Button icon>
-                    <Icon name='thumbs up' />
-                    Yes
+        <Image src= {image_URL} alt={title} wrapped ui={false}/>
+        <Card.Content>
+        <Card.Header>{title}</Card.Header>
+        <Card.Meta>
+            <span>{cuisine}</span> <br></br>
+            <span className='date'>{time} Minutes</span><br></br>
+            <span>{difficulty}</span>
+        </Card.Meta>
+        </Card.Content>
+        <Card.Content extra>
+            <div>
+                <Button  as='div' labelPosition='right'>
+                    <Button icon>
+                        <Icon name='thumbs up' />
+                        Yes
+                    </Button>
+                    <Label as='a' basic pointing='left'>
+                            2,048
+                    </Label>
                 </Button>
-                <Label as='a' basic pointing='left'>
-                         2,048
-                </Label>
-            </Button>
-            <Button as='div' labelPosition='left'>
-            <Label as='a' basic pointing='right'>
-                2,048
-            </Label>
-            <Button icon>
-                <Icon name='thumbs down' />
-                Mess
-            </Button>
-         </Button>
-        </div>
+                <Button as='div' labelPosition='left'>
+                    <Label as='a' basic pointing='right'>
+                        2,048
+                    </Label>
+                    <Button icon>
+                        <Icon name='thumbs down' />
+                        Mess
+                    </Button>
+                </Button>
+            </div>
+        </Card.Content>
     </Card>
 )
 }
